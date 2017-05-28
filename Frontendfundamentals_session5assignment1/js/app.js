8b2106dc04c350eb54d8487b11a8510838e4d4e2
balramravi1987 <balramravi1987@gmail.com>
2017-05-28 22:59:37 +0530

    Add files via upload

---
+++ Frontendfundamentals_session5assignment1/js/app.js
@@ -0,0 +1,5 @@
+var name = "Divya Bharathi";
+var age  = 26;
+var dob = "10 Jan 1991";
+var pob = "Chennai";
+console.log("Name: "+ name + "\nAge: "+ age +"\nDate of birth: " + dob + "\nPlace of birth: " + pob );
